f284bdffac39fc4fdc8234ff258f02ee27bb4dea
saylaww <saylaww@gmail.com>
2021-02-02 12:34:54 +0500

    For the test

---
+++ src/main/java/com/example/sprintest/SprintestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintest/SprintestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
